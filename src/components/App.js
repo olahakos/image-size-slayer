75f4e7de8aa4795450c9c28cd1b1fb1c4a154600
Akos Olah <Akos.Olah@viaplay.com>
2017-02-11 13:03:44 +0100

    Add dropzone

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,38 @@
 import React from 'react';
-import YeomanImage from './YeomanImage';
+import Dropzone from 'react-dropzone';
+import debugLib from 'debug';
+
+// import YeomanImage from './YeomanImage';
 import './app.css';
 
+const debug = debugLib('AppComponent:log');
+
 class AppComponent extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { files: [] };
+  }
+
+  onDrop(files) {
+    this.setState({ files }, () => {
+      debug('Accepted files: ', this.state.files);
+    });
+  }
 
   render() {
     return (
       <div className="index">
-        <YeomanImage />
-        <div className="notice">
-          Please edit <code>src/components/App.js</code> to get started!
-        </div>
+        <Dropzone onDrop={files => this.onDrop(files)}>
+          <div>Try dropping some files here, or click to select files to upload.</div>
+        </Dropzone>
+        {this.state.files ? <div>
+          <h2>Uploading {this.state.files.length} files...</h2>
+          <div>{this.state.files.map((file) => {
+            debug('here', file);
+            return <img alt="Preview" src={file.preview} />;
+          })}</div>
+        </div> : null}
       </div>
     );
   }
