20e79d1b4a459a09963ea7c1d4277a5eb4e5d299
Akos Olah <Akos.Olah@viaplay.com>
2017-02-11 20:15:00 +0100

    Add fake config

--- conf/config.js
+++ conf/config.js
@@ -1,4 +1,5 @@
 const config = {
-  google_api_key: process.env.GOOGLE_API_KEY || 'xxxxx'
+  google_api_key: process.env.GOOGLE_API_KEY || 'xxxxx',
+  bing_api_key: process.env.BING_API_KEY || 'xxxxxx',
 };
 export default config;
